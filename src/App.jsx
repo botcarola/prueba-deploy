a9c336a97aa42843f00ecb06b8503c7952337127
botcarola <celerner4@gmail.com>
2024-06-18 20:51:36 -0300

    desarrollo home

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>
+      Veterinaria
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum placeat fugit eum minus, cumque molestias commodi nemo, natus officia itaque nobis laborum beatae, fugiat qui illo voluptates earum quo deleniti corrupti libero maxime eos iste optio. Possimus adipisci exercitationem vel hic culpa, laudantium eius unde accusantium veritatis laborum, distinctio nisi iusto vitae provident maiores impedit perferendis illum, esse voluptate minus temporibus nulla sunt! Architecto eaque nemo, quos id, repellendus minima non error placeat ex sapiente suscipit, libero consequatur cum sequi unde doloremque praesentium fugiat repudiandae numquam exercitationem. Similique consectetur deleniti repudiandae esse ipsum veniam, vero recusandae eligendi consequuntur iure quae.
+    </p>      
     </>
   )
 }
